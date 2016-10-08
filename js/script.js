89a94aca9f97529a1c17f8719adae5873794d63a
dhollaway <hollaway.david@gmail.com>
2016-10-08 10:40:45 -0700

    Uncaught Error Bug related to lines 59, 60
    I believe this issue is related to the face that the 'citation' and
    'year' class is inside of a <span> that is inside of a <p>.

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ document.getElementById('loadQuote').addEventListener("click", printQuote, false
 //Helpful videos: https://teamtreehouse.com/library/javascript-loops-arrays-and-objects/tracking-data-using-objects/mixing-and-matching-arrays-and-objects
 //................https://teamtreehouse.com/library/javascript-basics/creating-reusable-code-with-functions/getting-information-from-a-function
 
+//contains the list of quotes.
 var quotes =[
 	 {quote :"The journey of a thousand miles starts with a singel step",
 	 source:"Chinese Proverb",
@@ -16,43 +17,52 @@ var quotes =[
 	 
 	 {quote :"If a tree fell and it wasn't posted on Facebook, did it really fall?",
 	 source:"David Hollaway",
-	 citation: "me!",
+	 citation: "from my mouth!",
 	 year: "2016"
 	 },
 	 
 	 {quote :"Cause Brawndo's got electrolytes.",
 	 source:"Secretary of Defense",
-	 citation: "From the Movie Idiocracy 1",
+	 citation: "1st from the Movie Idiocracy",
 	 year: "2006"
 	 },
 	 
 	 {quote :"Welcome to Costco.  I love you.",
 	 source:"Greeter at Costco",
-	 citation: "From the Movie Idiocracy 2",
+	 citation: "2nd from the Movie Idiocracy",
 	 year: "2006"
 	 },
 	 
 	 {quote :"I can't believe you like money too. We should hang out.",
 	 source:"Frito",
-	 citation: "From the Movie Idiocracy 3",
+	 citation: "3rd the Movie Idiocracy",
 	 year: "2006"
 	 }
 	 
 ];
 
+// Makes the random number according to the length of the array.
 var makeRandomNumber = Math.round(Math.random() * quotes.length);
 
 //2) Create a function named getRandomQuote which, a) selects a random quote object from the quotes array, 2) returns the randomly selected quote object
 function getRandomQuote(){
 	
 	var showQuote =	quotes[makeRandomNumber].quote;
-	//document.getElementsByClassName("quote").item(messageQuote);
-	//document.getElementsByClassName("quote")[0].length;
-	document.getElementsByClassName("quote")[0].innterHTM = "Test";
+	var showSource = quotes[makeRandomNumber].source;
+	var showCitation = quotes[makeRandomNumber].citation;
+	var showYear = quotes[makeRandomNumber].year;
 
-	//console.log(document.getElementsByClassName("quote")[0]);
-	//console.log(messageQuote);
+	document.getElementsByClassName("quote")[0].innerHTML = showQuote;
+	document.getElementsByClassName("source")[0].innerHTML = showSource;
+	//problem starts with the two lines (59, 60) below. Gets an uncaught TypeError scrip.js:57
+	//"Cannot read property of 0 or null"
+	document.getElementsByClassName("citation")[0].innerHTML = showCitation;
+	document.getElementsByClassName("year")[0].innerHTML = showYear;
+
 	console.log(showQuote);
+	console.log(showSource);
+	console.log(showCitation);
+	console.log(showYear);
 	//return showQuoteNow;
 }
 
@@ -61,7 +71,6 @@ function getRandomQuote(){
 function printQuote (){
 	
 	getRandomQuote();
-	//<p class="quote"> [quote here] </p> <p class="source"> [source here] <span class="citation"> [citation here] </span> <span class="year"> [year here] </span> </p>
 	
 }
 
